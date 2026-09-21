0697a6e9e0ff130abec9de3ad90cfee91060e1bc
MikeKtr <michal.kotra2000@gmail.com>
2026-09-21 23:28:51 +0200

    part 2

--- gpt2.py
+++ gpt2.py
@@ -15,11 +15,11 @@ class CausalSelfAttention(nn.Module):
 
     def __init__(self, config):
         super().__init__()
-        # making sure that the nr of naurons in passed layer can be split evenly to all heads
+        # making sure that the nr of neurons in passed layer can be split evenly to all heads
         assert config.n_embd % config.n_head == 0
-        # creating space for Key,Querry and Value for every nauron
+        # creating space for Key, Query and Value for every neuron
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
-        # creating space for agregating the results
+        # creating space for aggregating the results
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
         self.c_proj.NANOGPT_SCALE_INIT = 1
         self.n_head = config.n_head
@@ -27,17 +27,17 @@ class CausalSelfAttention(nn.Module):
 
     def forward(self, x):
         B, T, C = x.size() # Batch Time Channels
-        # input is projected ontu c_attn
+        # input is projected onto c_attn
         qkv = self.c_attn(x)
-        # qkv gets splitted onto seperate objects for q,k and v
+        # qkv gets split into separate objects for q, k and v
         q, k, v = qkv.split(self.n_embd, dim=2)
-        # changing the way matlab is seeing k q and v
+        # reshapes the tensors for k q and v
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) 
         q = q.view(B, T, self.n_head, C // self.n_head).transpose(1, 2) 
         v = v.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
-        # softmax 
+        # scaled dot-product attention
         y = F.scaled_dot_product_attention(q, k, v, is_causal=True) 
-        # reducing the matrix into the lower triangle matrix 
+        # reassembles all attention heads back into a single representation per each token
         y = y.transpose(1, 2).contiguous().view(B, T, C) 
         # projecting the matrix onto the final result
         y = self.c_proj(y)
@@ -48,15 +48,15 @@ class MLP(nn.Module):
         super().__init__()
         # Here you create 4 times more neurons so the model can 'think' for a while about the connections it made 
         self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
-        # GELU algorithim is a activation function similar to RELU, it is diferent because it doesn't alway turns off the negative values
+        # GELU algorithm is an activation function similar to RELU, it is different because it doesn't always turn off the negative values
         self.gelu = nn.GELU(approximate= 'tanh')
-        # Here you go back to original number of naurons
+        # Here you go back to original number of neurons
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
         self.c_proj.NANOGPT_SCALE_INIT = 1
         
 
     def forward(self,x):
-        # Here the model goes thrue all defined layers
+        # Here the model goes through all feed-forward layers
         x = self.c_fc(x)
         x = self.gelu(x)
         x = self.c_proj(x)
@@ -67,13 +67,13 @@ class MLP(nn.Module):
 class Block(nn.Module):
     def __init__(self,config):
         super().__init__()
-        # this layer is supposed to normalize the values of token along side the trait axis so the atteintion block works properly
+        # this layer is supposed to normalize the values of token along side the trait axis so the attention block works properly
         self.ln_1 = nn.LayerNorm(config.n_embd)
         # attention block
         self.attn = CausalSelfAttention(config)
         # another normalization layer
         self.ln_2 = nn.LayerNorm(config.n_embd)
-        # GELU function
+        # Feed-forward network (MLP)
         self.mlp = MLP(config)
 
     def forward(self,x):
@@ -87,14 +87,14 @@ class GPT(nn.Module):
 
     def __init__(self,config):
         super().__init__()
-        # config defined at the begining
+        # config defined at the beginning
         self.config = config
 
         # Here are defined all the final traits that each token will have
         self.transformer = nn.ModuleDict(dict(
-            # Relations with other tokens
+            # maps tokens into vector representations
             wte = nn.Embedding(config.vocab_size, config.n_embd),
-            # Relations with other positions
+            # map tokens into positional vectors
             wpe = nn.Embedding(config.block_size, config.n_embd),
             # hidden layers
             h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
@@ -103,20 +103,22 @@ class GPT(nn.Module):
 
         ))
 
-        # layer that project attention block from every token from n_embd size to vocab_size
+        # layer that projects final hidden states from n_embd size to vocab_size
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias = False)
 
-        # enforcking shared buffers
+        # enforcing weight tying (sharing weights between wte and lm_head)
         self.transformer.wte.weight = self.lm_head.weight
 
         # custom weight init
         self.apply(self._init_weights)
 
-    # custom weight inicialization, ranom weights with standard diviation = std.
+    # custom weight initialization, random weights with standard deviation = 0.02.
     def _init_weights(self,module):
         std = 0.02
+        # as for every step x = x + layer(x) the values would grow too fast, it is slowed down here
         if hasattr(module,'NANOGPT_SCALE_INIT'):
             std *= (2 * self.config.n_layer) ** -0.5
+        # for embedded weights the std is always 0.02 and for linear it can be different 
         if isinstance(module, nn.Linear):
             torch.nn.init.normal_(module.weight,mean=0.0,std = std)
             if module.bias is not None:
@@ -125,23 +127,34 @@ class GPT(nn.Module):
             torch.nn.init.normal_(module.weight,mean = 0.0, std = 0.02)
 
 
-    
+    # main forward function 
     def forward(self, idx, targets=None):
+        # batch, time
         B, T = idx.size()
+
         assert T <= self.config.block_size, f"Cannot forward sequence of length {T}, block size is only {self.config.block_size}"
+        # array [0,1,...,T-1] for every word's position      
         pos = torch.arange(0, T, dtype=torch.long, device=idx.device)
+        # passing pos through the embedding table [pos x traits]
         pos_emb = self.transformer.wpe(pos)
+        # passing idx through the embedding table [batch x time x trait]
         tok_emb = self.transformer.wte(idx)
+        # adding both of them together so now each token in batch can correlate with different particular tokens on different positions
         x = tok_emb + pos_emb
+        # passing the relations through the hidden layers
         for block in self.transformer.h:
             x = block(x)
+        # passing the relations through normalization layer
         x = self.transformer.ln_f(x)
+        # here the model compares all the traits to all the tokens 
         logits = self.lm_head(x)
+        # calculating loss 
         loss = None
         if targets is not None:
             loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
         return logits, loss
 
+    # downloading pretrained model from Hugging face
     @classmethod
     def from_pretrained(cls, model_type):
         assert model_type in {'gpt2', 'gpt2-medium', 'gpt2-large', 'gpt2-xl'}
@@ -181,6 +194,7 @@ class GPT(nn.Module):
                     sd[k].copy_(sd_hf[k])
 
         return model
+
 
     def configure_optimizers(self, weight_decay, learning_rate, device_type):
         param_dict = {pn: p for pn, p in self.named_parameters()}
@@ -241,18 +255,22 @@ class DataLoaderLite:
 
 
 train_loader = DataLoaderLite(4,32)
+torch.set_float32_matmul_precision('high')
 
 model = GPT(GPTConfig())
 model.to(device)
+model = torch.compile(model)
+
 
 optimizer = torch.optim.AdamW(model.parameters(),lr=3e-4)
 
-# 
+# Main loop of learning 
 for i in range(50):
     x,y = train_loader.next_batch()
     x,y = x.to(device), y.to(device)
     optimizer.zero_grad()
-    logits,loss = model(x,y)
+    with torch.autocast(device_type=device, dtypte=torch.bfloat16)
+        logits,loss = model(x,y)
     loss.backward()
     optimizer.step()
     print(f"step: {i}, loss: {loss.item()}")
